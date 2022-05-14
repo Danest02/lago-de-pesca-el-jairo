31be44d01b6c0a568f45a21da976290480131bf1
Daniel Botero <danes.daesboar02@gmail.com>
2022-05-13 22:08:21 -0500

    por aqui cvamos bien

--- src/js/navbar-header.js
+++ src/js/navbar-header.js
@@ -7,20 +7,20 @@ let initialScroll = window.pageYOffset;
 
 
 
-menuLinks.forEach(element => {
-        let href = element.getAttribute("href")
-        if(href !== "/"){
-            if (urlActual.includes(href)) {
-                element.classList.add("menu__link--active")
-                document.getElementById("menu__link--Inicio").classList.remove("menu__link--active")
-            } else {
-                element.classList.remove("menu__link--active")
-            }
-        }
+// menuLinks.forEach(element => {
+//         let href = element.getAttribute("href")
+//         if(href !== "/"){
+//             if (urlActual.includes(href)) {
+//                 element.classList.add("menu__link--active")
+//                 document.getElementById("menu__link--Inicio").classList.remove("menu__link--active")
+//             } else {
+//                 element.classList.remove("menu__link--active")
+//             }
+//         }
 
 
-    }
-)
+//     }
+// )
 hamburguer.addEventListener("click", () => {
 
     document.querySelector(".hamburguer__div-center").classList.toggle("hamburguer--simple")
@@ -33,23 +33,23 @@ hamburguer.addEventListener("click", () => {
 
 })
 
-window.addEventListener("scroll", () => {
-    if (!navbarHeader.classList.contains("navbar-header--hamburguer")) {
-        let currentScroll = window.pageYOffset;
-        if (currentScroll >= initialScroll && currentScroll >= mainScroll) {
-            navbarHeader.classList.add("navbar-header--hide")
-            navbarHeader.classList.add("navbar-header--scroll")
-        } else if (currentScroll <= mainScroll) {
-            navbarHeader.classList.add("navbar-header--hide")
-        } else {
-            navbarHeader.classList.remove("navbar-header--hide")
-        }
-        if (currentScroll == 0) {
-            navbarHeader.classList.remove("navbar-header--scroll")
-            navbarHeader.classList.remove("navbar-header--hide")
-        }
+// window.addEventListener("scroll", () => {
+//     if (!navbarHeader.classList.contains("navbar-header--hamburguer")) {
+//         let currentScroll = window.pageYOffset;
+//         if (currentScroll >= initialScroll && currentScroll >= mainScroll) {
+//             navbarHeader.classList.add("navbar-header--hide")
+//             navbarHeader.classList.add("navbar-header--scroll")
+//         } else if (currentScroll <= mainScroll) {
+//             navbarHeader.classList.add("navbar-header--hide")
+//         } else {
+//             navbarHeader.classList.remove("navbar-header--hide")
+//         }
+//         if (currentScroll == 0) {
+//             navbarHeader.classList.remove("navbar-header--scroll")
+//             navbarHeader.classList.remove("navbar-header--hide")
+//         }
 
-        initialScroll = currentScroll;
-    }
+//         initialScroll = currentScroll;
+//     }
 
-});
+// });
